b1870f8ae37bf1ed857a44cfdf1c7c32e5db8950
WnndGws <WnndGws@users.noreply.github.com>
2021-06-22 17:16:57 +0800

    Newsboat bookmarks allow me to extract title Can then do funcky stuff with it

---
+++ newsboat/.config/newsboat/bookmark_plugin.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+##
+
+url="$1"
+title="$2"
+description="$3"
+feed_title="$4"
+
+#echo -e "${url}\t${title}\t${description}\t${feed_title}" >> ~/bookmarks.txt
+
+if [[ "$url" == *"i.redd"* || "$url" == *"imgur"* ]]; then
+  wget --output-document /tmp/feh "$url"
+  convert /tmp/feh -background black -fill white -font Source-Code-Pro-Bold -pointsize 35 label:"$title" -gravity Center -append /tmp/feh_title
+  feh --scale-down --recursive /tmp/feh_title && /bin/rm /tmp/feh /tmp/feh_title
+else
+  "$HOME/git/scripts/shell/rofi_openwith" "$url"
+fi
