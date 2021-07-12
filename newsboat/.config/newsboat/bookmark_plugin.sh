f569b1f0eb86b9ffbdeb4c88b92a39af4844b71f
WnndGws <WnndGws@users.noreply.github.com>
2021-07-12 09:01:24 +0800

    Ignore males

--- newsboat/.config/newsboat/bookmark_plugin.sh
+++ newsboat/.config/newsboat/bookmark_plugin.sh
@@ -12,6 +12,8 @@ if [[ "$url" == *"i.redd"* || "$url" == *"imgur"* ]]; then
   wget --output-document /tmp/feh "$url"
   convert /tmp/feh -background black -fill white -font Source-Code-Pro-Bold -pointsize 35 label:"$title" -gravity Center -append /tmp/feh_title
   feh --scale-down --recursive /tmp/feh_title && /bin/rm /tmp/feh /tmp/feh_title
+elif [[ "$url" == *"v.red"* || "$url" == *"streama"* ]]; then
+  mpv --pause --quiet "$url"
 else
   "$HOME/git/scripts/shell/rofi_openwith" "$url"
 fi
