cc39b75646ebcce7fa35b8d23ed6e69b79904935
WnndGws <WnndGws@users.noreply.github.com>
2024-04-23 13:12:08 +0800

    Cant overwrite zz since i use it in these

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -11,7 +11,8 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- Save and commit
-keymap.set("n", "<leader>ww", ":w | Git add . | Git commit<CR> | :q", { desc = "Write and commit a file" })
+keymap.set("n", "<leader>ww", ":write | Git add . | Git commit<CR>", { desc = "Write and commit a file" })
+keymap.set("n", "<leader>wc", ":write", { desc = "Write and commit a file" })
 
 ----------------------
 --- Plugin Specific---
