705af2b00878b0aef7adc9691611161c2184e53d
Wynand Gouws <wynand@archbeast>
2024-07-03 14:56:47 +1000

    Add markdown rendering

--- neovim/.config/nvim/lua/plugins/aerial.lua
+++ neovim/.config/nvim/lua/plugins/aerial.lua
@@ -4,10 +4,16 @@ Plugin.event = "VeryLazy"
 Plugin.opts = function()
 	local opts = {
 		attach_mode = "global",
-		backends = { "lsp", "treesitter", "markdown", "man" },
+		backends = { "treesitter"},
 		show_guides = true,
 		layout = {
-
+            -- These control the width of the aerial window.
+            -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+            -- min_width and max_width can be a list of mixed types.
+            -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+            max_width = { 40, 0.2 },
+            width = nil,
+            min_width = { 40, 0.2 },
 			resize_to_content = false,
 			win_opts = {
 
@@ -16,13 +22,13 @@ Plugin.opts = function()
 				statuscolumn = " ",
 			},
 		},
-    -- stylua: ignore
-    guides = {
-    mid_item   = "├╴",
-    last_item  = "└╴",
-    nested_top = "│ ",
-    whitespace = "  ",
-    },
+        -- stylua: ignore
+        guides = {
+            mid_item   = "├╴",
+            last_item  = "└╴",
+            nested_top = "│ ",
+            whitespace = "  ",
+        },
 	}
 	return opts
 end
---
+++ neovim/.config/nvim/lua/plugins/markdown_render.lua
@@ -0,0 +1,28 @@
+local Plugin = { "MeanderingProgrammer/markdown.nvim" }
+
+Plugin.dependencies = {
+        'nvim-treesitter/nvim-treesitter', -- Mandatory
+        'nvim-tree/nvim-web-devicons', -- Optional but recommended
+    }
+
+Plugin.config = function()
+    local md = require('render-markdown')
+    md.setup({file_types = { 'markdown'}, headings = { '# ', '## ', '### ', '#### ', '##### ', '###### ' },
+    highlights = {
+        heading = {
+            -- Background of heading line
+            backgrounds = { 'NONE', 'NONE', 'NONE' },
+            -- Foreground of heading character only
+            foregrounds = {
+                'markdownH1',
+                'markdownH2',
+                'markdownH3',
+                'markdownH4',
+                'markdownH5',
+                'markdownH6',
+            },
+        },
+    },})
+end
+
+return Plugin
