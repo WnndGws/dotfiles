eda556ad746c20a7cdfff388045666a32dc4a31e
Wynand Gouws <wynand@archbeast>
2025-04-03 12:27:19 +1100

    add script

---
+++ waybar/.config/waybar/battery_brightness.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+## Change laptop brightness to match battery percentage
+
+BAT="$XDG_CONFIG_HOME/waybar/slstatus_battery_perc -1"
--- waybar/.config/waybar/check_updates.sh
+++ waybar/.config/waybar/check_updates.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/zsh
-""" Creates a FIFO for recording updates so it refreshes when i manually update """
+### Creates a FIFO for recording updates so it refreshes when i manually update
+
+BAR_FIFO='/tmp/waybar-updates'
+
+# If an old FIFO exists, remove it and create a new one
+[ -e "$BAR_FIFO" ] && rm "$BAR_FIFO"
+mkfifo "$BAR_FIFO"
+
+# Kill any running waybar-update process
+killall -9 waybar-updates
+
+# Send info to the FIFO
+waybar-updates --format '{aur}  {pacman}' --interval 3600 2> /dev/null > /tmp/waybar-updates &
+
+# Output the FIFO
+while read -r line; do
+    printf "%s\n" "$line"
+done < /tmp/waybar-updates
