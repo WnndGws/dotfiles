0da62e719afa5369cd83a3787f7b96ffb0eef9a8
Wynand Gouws <wynand@archbeast>
2025-05-27 13:30:55 +1000

    put workspaces on monitors and waybar

--- hyprland/.config/hypr/easyland_config.py
+++ hyprland/.config/hypr/easyland_config.py
@@ -14,6 +14,7 @@ listeners = {
 def init():
     set_monitors()
     set_workspaces()
+    run_waybar()
 
 
 ###############################################################################
@@ -22,56 +23,54 @@ def init():
 ###############################################################################
 
 
-"""
-def idle_config():
-    return [
-        [150, ["brightnessctl -s set 0"], ["brightnessctl -r"]],
-        [600, ["pidof hyprlock || hyprlock"]],
-        [720, ["hyprctl dispatch dpms off"], ["hyprctl dispatch dpms on"]],
-    ]
-
-
-###############################################################################
-# Handler of Hyprland IPC events
-# List of events: https://wiki.hyprland.org/IPC/
-###############################################################################
-
-
-def on_hyprland_event(event, argument):
-    if event in ["monitoradded", "monitorremoved", "configreloaded"]:
-        logger.info("Handling hyprland event: " + event)
-        set_monitors()
-
-        ## When disconnecting my laptop, a new workspace is created. We switch back to a default workspace
-        if event == "monitorremoved":
-            command.exec("hyprctl dispatch workspace 5", True)
-
-        ## Sometimes, Waybar or wpaperd crashes
-        if event in ["monitoradded", "monitorremoved"]:
-            command.exec("pkill waybar || true && waybar", background=True)
-            command.exec("pkill wpaperd || true && wpaperd -d", background=True)
-
-
-###############################################################################
-# Handlers of Systemd logind events
-###############################################################################
-
-
-def on_PrepareForSleep(payload):
-    if "true" in payload:
-        logger.info("Locking the screen before suspend")
-        command.exec("pidof hyprlock || hyprlock", True)
-
-
-# To use this handler, you need to launch your locker (hyprlock or swaylock) like this: hyprlock && loginctl unlock-session
-# def on_Unlock():
-#     logger.info("Unlocking the screen")
-
-# To use this handler, you need to launch your locker like this: loginctl lock-session
-# def on_lock():
-#     logger.info("Locking the screen")
-"""
-
+# def idle_config():
+# return [
+# [150, ["brightnessctl -s set 0"], ["brightnessctl -r"]],
+# [600, ["pidof hyprlock || hyprlock"]],
+# [720, ["hyprctl dispatch dpms off"], ["hyprctl dispatch dpms on"]],
+# ]
+#
+#
+# ###############################################################################
+# # Handler of Hyprland IPC events
+# # List of events: https://wiki.hyprland.org/IPC/
+# ###############################################################################
+#
+#
+# def on_hyprland_event(event, argument):
+# if event in ["monitoradded", "monitorremoved", "configreloaded"]:
+# logger.info("Handling hyprland event: " + event)
+# set_monitors()
+#
+# ## When disconnecting my laptop, a new workspace is created. We switch back to a default workspace
+# if event == "monitorremoved":
+# command.exec("hyprctl dispatch workspace 5", True)
+#
+# ## Sometimes, Waybar or wpaperd crashes
+# if event in ["monitoradded", "monitorremoved"]:
+# command.exec("pkill waybar || true && waybar", background=True)
+# command.exec("pkill wpaperd || true && wpaperd -d", background=True)
+#
+#
+# ###############################################################################
+# # Handlers of Systemd logind events
+# ###############################################################################
+#
+#
+# def on_PrepareForSleep(payload):
+# if "true" in payload:
+# logger.info("Locking the screen before suspend")
+# command.exec("pidof hyprlock || hyprlock", True)
+#
+#
+# # To use this handler, you need to launch your locker (hyprlock or swaylock) like this: hyprlock && loginctl unlock-session
+# # def on_Unlock():
+# #     logger.info("Unlocking the screen")
+#
+# # To use this handler, you need to launch your locker like this: loginctl lock-session
+# # def on_lock():
+# #     logger.info("Locking the screen")
+#
 ###############################################################################
 # Various methods
 ###############################################################################
@@ -108,3 +107,11 @@ def set_workspaces():
         command.exec('hyprctl keyword workspace "8, monitor:HDMI-A-1"')
         command.exec('hyprctl keyword workspace "9, monitor:HDMI-A-1"')
         command.exec('hyprctl keyword workspace "10, monitor:HDMI-A-1"')
+
+
+def run_waybar():
+    logger.info("Setting monitors")
+    if hostname == "arch-beast":
+        command.exec("waybar --config ~/.config/waybar/beast_config.jsonc")
+    else:
+        command.exec("waybar --config ~/.config/waybar/laptop_config.jsonc")
