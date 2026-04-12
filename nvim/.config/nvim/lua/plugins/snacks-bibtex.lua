560ad26102ace3db1abc534dec08bdef4a9aa900
Wynand Gouws <WnndGws@users.noreply.github.com>
2026-04-12 15:58:12 +1000

    20260412_155812

--- nvim/.config/nvim/lua/plugins/snacks-bibtex.lua
+++ nvim/.config/nvim/lua/plugins/snacks-bibtex.lua
@@ -2,7 +2,7 @@ return {
   "krissen/snacks-bibtex.nvim",
   dependencies = { "folke/snacks.nvim" },
   opts = {
-    -- global_files = { "~/Documents/library.bib" },
+    global_files = { "~/git/git-wiki/references.bib"},
   },
   keys = {
     { "<leader>bc", function() require("snacks-bibtex").bibtex() end, desc = "BibTeX citations" },
