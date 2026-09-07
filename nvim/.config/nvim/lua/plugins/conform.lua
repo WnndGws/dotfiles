25181c2e5eaccabf5feccfeaba87d32434618d18
Wynand Gouws <WnndGws@users.noreply.github.com>
2026-09-07 20:24:07 +0800

    Add some linters and conform, and sort blocks

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,24 +1,28 @@
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre", "BufReadPre", "BufNewFile" },
+	lazy = false,
 	keys = {
 		-- No which-key needed
 	},
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
-				awk = { "gawk" },
 				angular = { "rustywind", "prettier" },
+				awk = { "gawk" },
 				bash = { "beautysh", "shellharden", "shellcheck" },
 				css = { "rustywind", "stylelint", "prettier" },
-				html = { "rustywind", "html_beautify", "prettier" },
+				html = { "djlint", "rustywind", "html_beautify", "prettier" },
 				javascript = { "rustywind", "prettier" },
+				jinja = { "djlint" },
 				json = { "jq", "fixjson", "prettier" },
-				tex = { "bibtex-tidy", "tex-fmt" },
+				latex = { "tex-fmt", "latexindent" },
 				lua = { "stylua" },
 				markdown = { "markdownlint-cli2", "mdslw", "prettier" },
-				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+				python = { "pyupgrade", "ruff_fix", "ruff_format", "ruff_organize_imports" },
 				sh = { "beautysh", "shellharden", "shellcheck" },
+				tex = { "bibtex-tidy", "tex-fmt", "latexindent" },
+				toml = { "taplo" },
 				typescript = { "rustywind", "prettier" },
 				xml = { "xmlstarlet" },
 				yaml = { "yamlfix", "yamlfmt", "prettier" },
@@ -26,14 +30,25 @@ return {
 			},
 			formatters = {
 				["markdownlint-cli2"] = {
-					"--fix",
-					"--config",
-					vim.fn.expand("~/.config/nvim/markdownlint_conf.json"),
+					args = {
+						"--fix",
+						"--config",
+						vim.fn.expand("~/.config/nvim/markdownlint_conf.json"),
+					},
 				},
 				["prettier"] = {
-					"--tab-width 4",
-					"--prose-wrap always",
-					"--write",
+					args = {
+						"--tab-width",
+						"4",
+						"--prose-wrap",
+						"always",
+						"--write",
+					},
+				},
+				["pyupgrade"] = {
+					command = "pyupgrade",
+					args = { "--py315-plus" },
+					stdin = false,
 				},
 			},
 			format_on_save = {
